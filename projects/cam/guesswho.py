09921a5579bfe8360b99332db6d5fc194eee6553
HTaylor7486 <htaylor7486@sohamcollege.org.uk>
2014-12-12 12:08:15 +0000

    guess who stuff

--- projects/cam/guesswho.py
+++ projects/cam/guesswho.py
@@ -1,4 +1,4 @@
-import picamera,time#imports moduals
+import picamera,time,json#imports moduals
 def getpic(name="null"):#defines function
     try:#trys to run code block
         with picamera.PiCamera() as camera:#runs code block with the pi cam
@@ -14,36 +14,35 @@ def getpic(name="null"):#defines function
         return filename
     except picamera.exc.PicameraMMALError:
         print("Your camera is not working please connect and restart the program")
-        
+
 def getchar():
-    name = "1"
+    name = ""
     while name == "":  
         name = input("what is your name?")
-    hair = "blonde"
+    hair = ""
     while not hair in ["blonde","brown","ginger","no hair"]:
         hair = input ("what hair colour do you have? (blonde/brown/ginger/no hair)")
-    hat = "y"
+    hat = ""
     while not hat in ["y","n"]:
         hat = input("do you have a hat? (y/n)")
-    eye = "green"
+    eye = ""
     while not eye in ["green","brown","blue"] :
         eye = input("what is your eye colour")
-    gender = "m"
+    gender = ""
     while not gender in ["m","f"] :
         gender = input("what is your gender?(m/f)")
-    fhair = "y"
+    fhair = ""
     while not fhair in ["y","n"]:
         fhair = input("do you have facial hair?(y/n)")
-    glass = "y"
+    glass = ""
     while not glass in ["y","n"]:
         glass = input("do you have glasses?(y/n)")
     charprof = [name,hair,hat,eye,gender,fhair,glass]
     getpic(name)
     return charprof
+        
 def save():
-    prof = getchar()
-    with open("profiles.txt",mode = "a", encoding = "utf-8") as my_file:
-        for item in prof:
-            my_file.write(item+",")
-        my_file.seek(-1)
-        my_file.write("\n")
+    charprof = getchar()
+    with open("profiles.txt",mode = "w") as my_file:
+        json.dump(charprof,my_file)
+save()
