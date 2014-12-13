dc130b4cfa5ba5dc0739a1771d41fc2582e1fbe6
HTaylor7486 <htaylor7486@sohamcollege.org.uk>
2014-12-13 17:04:03 +0000

    Update validation examples.py

--- projects/validation examples.py
+++ projects/validation examples.py
@@ -1,15 +1,15 @@
 #import time,random,picamera
-name = ""
-while name == "":
-    name = input("what is your name? ---")
+name = ""#sets name to nothing
+while name == "":#checks to see if name is = nothing
+    name = input("what is your name? ---")#sets name to user input
 #print("good morning " + name)
 #print("Good morning %s" %name)
 #print("Good morning {0}".format(name))
-password = "hi_there"
-guess = input("{0} do you know the password --- ".format (name))
-while guess != password: 
-    print("boooo")
-    guess = input("{0} do you know the password --- ".format (name))
+password = "hi_there"#sets password to hi_there
+guess = input("{0} do you know the password --- ".format (name))#sets guess to user input
+while guess != password: #checks to see if guess does not = password
+    print("boooo")#prints booooo
+    guess = input("{0} do you know the password --- ".format (name))#sets guess to user input
 else:
-    print("well done {0}".format (name))
+    print("well done {0}".format (name))#prints message
     
