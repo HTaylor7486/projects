0e8173518b8ef1ffd99084950c84c67b5677655b
HTaylor7486 <htaylor7486@sohamcollege.org.uk>
2014-12-13 16:49:47 +0000

    Update camtest.py

--- projects/camtest.py
+++ projects/camtest.py
@@ -1,9 +1,9 @@
-import picamera,time
-for run in range(5):
-    name = input('please enter your name')
-    with picamera.PiCamera() as camera:
-        camera.resolution = (1024,768)
-        camera.start_preview()
+import picamera,time#imports moduals
+for run in range(5):#sets run to 1-5 every time block runs
+    name = input('please enter your name')#sets name to user input
+    with picamera.PiCamera() as camera:#sets picam modual to camera
+        camera.resolution = (1024,768)#sets cam resalution
+        camera.start_preview()#starts preview
         
-        time.sleep(2)
-        camera.capture( name +'.jpeg')
+        time.sleep(2)#pauses the program
+        camera.capture( name +'.jpeg')#takes photo and names it to your name .jpeg
