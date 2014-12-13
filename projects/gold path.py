56ab9a03fb49677e51dde2006b9db0cccb2885c4
HTaylor7486 <htaylor7486@sohamcollege.org.uk>
2014-12-13 16:45:32 +0000

    Update gold path.py

--- projects/gold path.py
+++ projects/gold path.py
@@ -1,12 +1,12 @@
-import time,mcpi.minecraft as minecraft
-mc = minecraft.Minecraft.create()
-while True:
-    x,y,z = mc.player.getPos()
-    block = mc.getBlock(x,y-1,z)
+import time,mcpi.minecraft as minecraft#imports moduals
+mc = minecraft.Minecraft.create()#sets the minecraft modual to mc
+while True:#starts infinite loop
+    x,y,z = mc.player.getPos()#sets the players coords to x,y,z
+    block = mc.getBlock(x,y-1,z)#finds out what the block is below the player and sets it to block
     
-    if block == 0:
-        pass
+    if block == 0:#checks if block is = to 0
+        pass#does nothing
     else:
-        mc.setBlock(x,y-1,z,41)
+        mc.setBlock(x,y-1,z,41)#sets block to gold
         
     
